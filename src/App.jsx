6d7188320f6289279380a613979c8b27401eda6a
Mohammed Gouaouri <mohammed.degouaouri@namla.cloud>
2024-03-21 14:08:35 +0100

    feat: add leaderboard page with pagination buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignupPage from './pages/auth/Signup'
 import { Routes, Route } from "react-router-dom";
 import { Home } from './pages/home/Home';
 import Workspace from './pages/workspace/Workspace';
+import Leaderboard from './pages/leaderboard/Leaderboard';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/challenges' element={<Home />}/>
         <Route path='/workspace/:id' element={<Workspace />}/>
+        <Route path='/leaderboard' element={<Leaderboard />}/>
         <Route path='/signup' element={<SignupPage />}/>
         <Route path='/login' element={<SigninPage />}/>
       </Routes>
--- src/pages/leaderboard/Leaderboard.jsx
+++ src/pages/leaderboard/Leaderboard.jsx
@@ -1,8 +1,83 @@
+import { Link } from "react-router-dom";
+import { Navbar } from "../../components/Navbar";
+import { useState } from "react";
 
 function Leaderboard() {
+
+  // TODO: Handle load data
+
+  const [page, setPage] = useState(1)
+  const handleNextPage = () => {
+    setPage(page+1) // TODO: Handle page < maxPages
+  }
+  const handlePrevPage = () => {
+    page > 1 && setPage(page-1)
+  }
   return (
-    <div>Leaderboard</div>
-  )
+    <div className="h-screen w-screen overflow-scroll bg-slate-100">
+        <Navbar />
+        <div className="overflow-scroll shadow-md bg-gray-100 rounded-lg p-4">
+          
+          <table className="table-auto w-full text-gray-800">
+              {/* head */}
+              <thead className="bg-gray-200 text-center">
+                  <tr>
+                      <th className="px-4 py-2">Rank</th>
+                      <th className="px-4 py-2">Name</th>
+                      <th className="px-4 py-2">Score</th>
+                      <th className="px-4 py-2">Solved challenges</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {/* rows */}
+                  <tr className="border-b border-gray-300">
+                      <td className="px-4 py-2 flex justify-center">
+                          1
+                      </td>
+                      {/* TODO: Chanege this to real id */}
+                      <td><Link to={'/workspace/0'} className="px-4 py-2">Omar moukhfi</Link></td>
+                      <td className="px-4 py-2">300</td>
+                      <td className="px-4 py-2">140</td>
+                  </tr>
+
+                  {/* Add more rows as needed */}
+                  <tr className="border-b border-gray-300">
+                      <td className="px-4 py-2 flex justify-center">
+                          1
+                      </td>
+                      {/* TODO: Chanege this to real id */}
+                      <td><Link to={'/workspace/0'} className="px-4 py-2">Omar moukhfi</Link></td>
+                      <td className="px-4 py-2">300</td>
+                      <td className="px-4 py-2">140</td>
+                  </tr>
+
+                  <tr className="border-b border-gray-300">
+                      <td className="px-4 py-2 flex justify-center">
+                          1
+                      </td>
+                      {/* TODO: Chanege this to real id */}
+                      <td><Link to={'/workspace/0'} className="px-4 py-2">Omar moukhfi</Link></td>
+                      <td className="px-4 py-2">300</td>
+                      <td className="px-4 py-2">140</td>
+                  </tr>
+              </tbody>
+          </table>
+          
+      </div>
+      <div className="flex flex-row-reverse">
+        <div className="join m-2">
+              <button 
+                onClick={handlePrevPage}
+                className="join-item btn bg-slate-100 text-black hover:bg-textPrimary hover:text-white">«</button>
+              <button className="join-item btn bg-slate-100 text-black hover:bg-textPrimary hover:text-white">Page {page}</button>
+              <button 
+                onClick={handleNextPage}
+                className="join-item btn bg-slate-100 text-black hover:bg-textPrimary hover:text-white">»</button>
+        </div>
+      </div>
+    </div>
+    
+);
 }
 
 export default Leaderboard
